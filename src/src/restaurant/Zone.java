7e102f21d930c7ec5457db774ed6c8c5714e103f
Jay <Jay@n00b.com>
2014-06-23 12:02:58 +0700

    Keepok is a douche

--- src/src/restaurant/Zone.java
+++ src/src/restaurant/Zone.java
@@ -1,4 +1,19 @@
 package src.restaurant;
-public class Zone {
+import java.util.ArrayList;
+import java.util.List;
 
+import src.person.*;
+import src.account.*;
+
+public class Zone {
+	private List<IPerson> persons;
+	public Zone(){
+		persons = new ArrayList<IPerson>();
+		
+	}
+	public static getZoneByPerson(IPerson person){
+		IPerson returnObject = null;
+		
+		return returnObject;
+	}
 }
